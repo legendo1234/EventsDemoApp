a5c9d4bea7c6f82cec190ce8175c0d47956078b5
Matej Weber <matej.weber@gmail.com>
2024-02-17 13:46:15 +0100

    Add project files.

---
+++ EventsDemo/CollegeClassModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventsDemo
+{
+    public class CollegeClassModel
+    {
+        public event EventHandler<string> EnrollmentFull;
+
+        private List<string> enrolledStudents = new List<string>();
+        private List<string> waitingList = new List<string>();
+
+        public string CourseTitle { get;private set; }
+        public int MaximumStudents { get; private set; }
+
+        public CollegeClassModel(string title, int maximumStudents) 
+        { 
+            CourseTitle = title;
+            MaximumStudents = maximumStudents;
+        }
+
+        public string SignUpStudent(string studentName)
+        {
+            string output = "";
+
+            if (enrolledStudents.Count < MaximumStudents)
+            {
+                enrolledStudents.Add(studentName);
+                output = $"{studentName} was enroled in {CourseTitle}";
+                //Check to see if we are maxed out
+                if (enrolledStudents.Count == MaximumStudents)  
+                {
+                    EnrollmentFull.Invoke(this, $"{CourseTitle} enrollment is full.");
+                }
+            }
+            else
+            {
+                waitingList.Add(studentName);
+                output = $"{studentName} was added to the waiting list for {CourseTitle}";
+            }
+            return output ;
+        }
+    }
+}
---
+++ EventsDemo/ConsoleHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsDemo
+{
+    public static class ConsoleHelpers
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
